168ea01d2f448d869ceb010d6e847140e075e98d
gspinaci <spino9330@gmail.com>
2018-05-16 21:07:14 +0200

    Base file with multiple card lecture

--- base.py
+++ base.py
@@ -1,7 +1,9 @@
-import pyaudio 
+import pyaudio
 import wave
 import time
 import numpy
+import sys
+from module.leitor_cartao import LeitorCartao
 
 # instantiate PyAudio (1)
 p = pyaudio.PyAudio()
@@ -11,13 +13,15 @@ sound2 = wave.open("pizzica/guitar.wav", 'rb')
 
 volume1 = 0.0
 
+
 def callback(in_data, frame_count, time_info, status):
     data1 = sound1.readframes(frame_count)
     data2 = sound2.readframes(frame_count)
     decodeddata1 = numpy.fromstring(data1, numpy.int16)
     decodeddata2 = numpy.fromstring(data2, numpy.int16)
-    newdata = (decodeddata1 * 0.5 + decodeddata2* volume1).astype(numpy.int16)
+    newdata = (decodeddata1 * 0.5 + decodeddata2 * volume1).astype(numpy.int16)
     return (newdata.tostring(), pyaudio.paContinue)
+
 
 # open stream using callback (3)
 stream = p.open(format=p.get_format_from_width(sound1.getsampwidth()),
@@ -29,21 +33,32 @@ stream = p.open(format=p.get_format_from_width(sound1.getsampwidth()),
 # start the stream (4)
 stream.start_stream()
 
-# wait for stream to finish (5)
-while stream.is_active():
-    if volume1 < 0.5:
-        volume1 += 0.005
-    
-    if volume1 > 0.5:
-        volume1 = 0.5
+reader_card = LeitorCartao()
 
-    time.sleep(0.1)
+try:
+    while True:
+        if not reader_card.isAlive():
+            reader_card.start()
+except KeyboardInterrupt:
+    print "trl+C received! Sending kill to " + reader_card.getName()
+    if reader_card.isAlive():
+        reader_card._stopevent.set()
+
+# wait for stream to finish (5)
+# while stream.is_active():
+#    if volume1 < 0.5:
+#        volume1 += 0.005
+
+#    if volume1 > 0.5:
+#        volume1 = 0.5
+
+#    time.sleep(0.1)
 
 # stop stream (6)
-stream.stop_stream()
-stream.close()
-sound1.close()
-sound2.close()
+# stream.stop_stream()
+# stream.close()
+# sound1.close()
+# sound2.close()
 
 # close PyAudio (7)
-p.terminate()
+# p.terminate()
